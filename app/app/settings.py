38641e6932c15f440da5f1503bb1d1701a505762
Hyunminmax <hyunmin.shin81@gmail.com>
2024-03-27 14:29:48 +0900

    Youtube test

--- app/app/settings.py
+++ app/app/settings.py
@@ -148,10 +148,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -171,3 +167,13 @@ CHANNEL_LAYERS = {
         "BACKEND":"channels.layers.InMemoryChannelLayer"
     }
 }
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = '/static/static'
+MEDIA_URL = '/static/media'
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol.web/static'
